ede396f21f3dcb24a5a6f8a760a31cf0bae980b5
Leif Etter <etter.leif@gmail.com>
2025-04-10 19:02:33 +0200

    move index.ts to src folder and set up server initializing process

--- index.ts
+++
@@ -1,9 +0,0 @@
-async function main() {
-  try {
-    console.log("Running me");
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-main();
---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import "dotenv/config.js";
+import logger from "./logging/logger.js";
+import app from "./app.js";
+
+const abortMessage: string = "--- Aborting Initialization Process ---";
+const isNumbersOnly = (str: string): boolean => /^\d+$/.test(str);
+
+async function main() {
+  try {
+    logger.info("### Abipulli.com Backend Initializing ###");
+    const jwtSecret: string | undefined = process.env.JWT_SECRET;
+    const port: string | undefined = process.env.PORT;
+
+    if (jwtSecret == undefined) {
+      logger.error("Please set JWT_SECRET=[randomString] in the .env file.");
+      logger.warn(abortMessage);
+      return;
+    }
+    if (port == undefined || !isNumbersOnly(port)) {
+      logger.error(
+        "Invalid Port in .env. Please set a PORT=[port to be used] in the .env file."
+      );
+      logger.warn(abortMessage);
+      return;
+    }
+    app.listen(parseInt(port), () => {
+      logger.info(`### Listening to Port ${port} ###`);
+    });
+  } catch (error) {
+    logger.error(error);
+  }
+}
+
+main();
