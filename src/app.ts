294e9cee0ed288a86ce8066e53a674d78624f2d8
Leif Etter <etter.leif@gmail.com>
2025-04-15 12:38:59 +0200

    implement errorHandler Middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import { pinoHttp } from "pino-http";
 import logger from "./logging/logger";
-
+import router from "./route";
+import { apiErrorHandler } from "./error/errorMiddleware";
 const app = express();
 
 app.use(express.json());
@@ -13,6 +14,7 @@ app.use(
 );
 app.use(router);
 
+app.use(apiErrorHandler);
 
 app.route("/test").get((req: express.Request, res: express.Response): void => {
   res.status(200).send({ message: "Working" });
