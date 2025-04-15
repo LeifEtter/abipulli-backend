5d0487e7af777ddb34e904e5344c4dbd21cbf522
Leif Etter <etter.leif@gmail.com>
2025-04-15 12:32:22 +0200

    improve db initialization and incorporate relation file

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,6 +1,11 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import * as schema from "./schema";
+import * as relation from "./relation";
+import pg from "pg";
+const { Pool } = pg;
 
-const db = drizzle(process.env.DATABASE_URL!);
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const db = drizzle(pool, { schema: { ...schema, ...relation } });
 
 export default db;
