79705a81d6e35e4bc7646fcfe36c3c169985cf7a
Leif Etter <etter.leif@gmail.com>
2025-04-16 23:11:02 +0200

    create middleware for setting min role power and evaluating if user can access

---
+++ src/auth/authorization.ts
@@ -0,0 +1,13 @@
+import { errorMessages } from "constants/errorMessages";
+import ApiError from "error/ApiError";
+import { NextFunction, Request, Response } from "express";
+
+export const minPower =
+  (minPower: number) => (req: Request, res: Response, next: NextFunction) => {
+    const rolePower: number = res.locals.user.role_power;
+    if (rolePower >= minPower) {
+      next();
+    } else {
+      next(new ApiError({ code: 401, info: errorMessages.rolePowerTooLow }));
+    }
+  };
