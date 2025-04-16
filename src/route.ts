c1064079ccd0a5b756c0f86c731bbaeac1ed44aa
Leif Etter <etter.leif@gmail.com>
2025-04-16 23:12:02 +0200

    implement orders router

--- src/route.ts
+++ src/route.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import userRouter from "./components/user/user.route";
+import orderRouter from "./components/order/order.route";
 const router = Router();
 
 router.use("/user", userRouter);
+router.use("/order", orderRouter);
 
 export default router;
