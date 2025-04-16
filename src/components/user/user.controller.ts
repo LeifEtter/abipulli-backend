06ebf01a5de50c2d98cd2f9efce6f02054010e3a
Leif Etter <etter.leif@gmail.com>
2025-04-16 02:53:50 +0200

    create client for google sso

--- src/components/user/user.controller.ts
+++ src/components/user/user.controller.ts
@@ -18,6 +18,9 @@ import {
   getUserByEmail,
   passwordIsValid,
 } from "./user.util";
+import { OAuth2Client, TokenPayload } from "google-auth-library";
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 /** */
 /** Sign in Anonymous User */
