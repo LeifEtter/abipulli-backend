07dbc0e0fa061560cab7df06269043469a388369
Leif Etter <etter.leif@gmail.com>
2025-04-16 23:11:35 +0200

    add order util to get order by id

---
+++ src/components/order/order.util.ts
@@ -0,0 +1,10 @@
+import db from "db/db";
+import { orders, SelectOrder } from "db/schema";
+import { eq } from "drizzle-orm";
+
+export const getOrderById = async (
+  id: number
+): Promise<SelectOrder | undefined> => {
+  const order = await db.query.orders.findFirst({ where: eq(orders.id, id) });
+  return order;
+};
