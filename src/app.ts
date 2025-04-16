be5d9d293036eb6de0a3a19d234041f8d60b654f
Leif Etter <etter.leif@gmail.com>
2025-04-16 16:08:46 +0200

    use cookie parser

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import { pinoHttp } from "pino-http";
 import { httpLogger, logger } from "./logging/logger";
 import router from "./route";
 import { apiErrorHandler } from "./error/errorMiddleware";
+import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(express.json());
 
 app.use(httpLogger);
+app.use(cookieParser());
 app.use(router);
 
 app.use(apiErrorHandler);
