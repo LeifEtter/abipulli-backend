c41a4ad0ac706da7e7fa90e6afdaefbd98e17bcc
Leif Etter <etter.leif@gmail.com>
2025-04-16 16:09:46 +0200

    add better typing to errorMessages object for easier use

--- src/constants/errorMessages.ts
+++ src/constants/errorMessages.ts
@@ -2,7 +2,17 @@ export interface ErrorInfo {
   code: number;
   msg: string;
 }
-const errorMessages: { [index: string]: ErrorInfo } = {
+
+type ErrorName =
+  | "emailNotFound"
+  | "emailAlreadyRegistered"
+  | "faultyLoginCredentials"
+  | "faultySSOCredentials"
+  | "faultyToken"
+  | "missingToken"
+  | "rolePowerTooLow";
+
+export const errorMessages: Record<ErrorName, ErrorInfo> = {
   emailNotFound: {
     code: 0,
     msg: "Kein Nutzer mit dieser Email konnte gefunden werden",
@@ -19,10 +29,16 @@ const errorMessages: { [index: string]: ErrorInfo } = {
     code: 3,
     msg: "Dein Google IDToken ist ungültig",
   },
+  faultyToken: {
+    code: 4,
+    msg: "Der Token ist abgelaufen oder wurde ungültig gemacht. Bitte Logge dich nochmals ein",
+  },
+  missingToken: {
+    code: 5,
+    msg: "Deine Anfrage enthält keinen Token. Bitte füge einen 'jwt_token' als Cookie hinzu.",
+  },
+  rolePowerTooLow: {
+    code: 6,
+    msg: "Dein Power Level ist zu tief für Zugriff auf diesen Endpunkt.",
+  },
 };
-
-export type ErrorCode = keyof typeof errorMessages;
-
-export function getErr(errorCode: ErrorCode) {
-  return errorMessages[errorCode];
-}
