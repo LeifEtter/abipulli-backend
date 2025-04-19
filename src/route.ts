909a014f38c7d16293bea0e7cd4c978b1e3b3f79
Leif Etter <etter.leif@gmail.com>
2025-04-19 22:12:10 +0200

    add image route to router

--- src/route.ts
+++ src/route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import userRouter from "./components/user/user.route";
 import orderRouter from "./components/order/order.route";
+import imageRouter from "./components/image/image.route";
 const router = Router();
 
 router.use("/user", userRouter);
 router.use("/order", orderRouter);
+router.use("/image", imageRouter);
 
 export default router;
