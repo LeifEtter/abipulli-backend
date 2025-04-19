ca2c62116cb7ed8341d537f4c30890632f472ea2
Leif Etter <etter.leif@gmail.com>
2025-04-19 22:14:54 +0200

    improve order routes and incorporate new param validation middleware

--- src/components/order/order.route.ts
+++ src/components/order/order.route.ts
@@ -1,14 +1,18 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { orderCreateUpdateSchema } from "validation/schemas/orderSchemas";
-import { validateBody, validateParam } from "validation/validationMiddleware";
+import { validateBody, validateParams } from "validation/validationMiddleware";
 import { createOrder, deleteOrder, updateOrder } from "./order.controller";
 import { authenticate } from "auth/authentication";
 import { minPower } from "auth/authorization";
+import { saveImage } from "components/image/image.controller";
+import multer from "multer";
 
 const router: Router = Router();
 
+const multerClient = multer();
+
 router
-  .route("/create")
+  .route("/")
   .post(
     authenticate,
     minPower(1),
@@ -17,17 +21,32 @@ router
   );
 
 router
-  .route("/update/:id")
+  .route("/:id/image")
+  .post(
+    authenticate,
+    minPower(1),
+    validateParams({ requiredParams: ["id"] }),
+    multerClient.single("image"),
+    saveImage
+  );
+
+router
+  .route("/:id")
   .patch(
     authenticate,
     minPower(1),
     validateBody(orderCreateUpdateSchema),
-    validateParam,
+    validateParams({ requiredParams: ["id"] }),
     updateOrder
   );
 
 router
-  .route("/delete/:id")
-  .delete(authenticate, minPower(1), validateParam, deleteOrder);
+  .route("/:id")
+  .delete(
+    authenticate,
+    minPower(1),
+    validateParams({ requiredParams: ["id"] }),
+    deleteOrder
+  );
 
 export default router;
