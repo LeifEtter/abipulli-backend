1294bca2d85d9d5d9c9747a1d66b0422c52d384c
Leif Etter <etter.leif@gmail.com>
2025-04-22 13:32:58 +0200

    add resource property to apiError and add static constructor for 404 errors

--- src/error/ApiError.ts
+++ src/error/ApiError.ts
@@ -1,17 +1,20 @@
-import type { ErrorInfo } from "./errorMessages";
+import { errorMessages, type ErrorInfo } from "./errorMessages";
 
 interface ApiErrorParams {
   code: number;
   info: ErrorInfo;
+  resource?: string;
 }
 
 class ApiError {
   code: number;
   info: ErrorInfo;
+  resource?: string;
 
-  constructor({ code, info }: ApiErrorParams) {
+  constructor({ code, info, resource }: ApiErrorParams) {
     this.code = code;
     this.info = info;
+    this.resource = resource;
   }
 
   static badRequest(errorInfo: ErrorInfo) {
@@ -20,6 +23,14 @@ class ApiError {
 
   static internal(errorInfo: ErrorInfo) {
     return new ApiError({ code: 500, info: errorInfo });
+  }
+
+  static notFound(resource: string) {
+    return new ApiError({
+      code: 404,
+      info: errorMessages.resourceNotFound,
+      resource,
+    });
   }
 }
 
