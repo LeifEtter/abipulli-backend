df3cf3eea1507abe64391e72712cd46931c6e583
Leif Etter <etter.leif@gmail.com>
2025-04-22 13:17:43 +0200

    add table in schema for text elements

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -121,6 +121,28 @@ export const designs = pgTable(
 export type InsertDesign = typeof designs.$inferInsert;
 export type SelectDesign = typeof designs.$inferSelect;
 
+export const textElements = pgTable(
+  "text_elements",
+  {
+    id: serial().notNull().primaryKey(),
+    design_id: integer(),
+    content: varchar().notNull(),
+    font: varchar(),
+    position_x: real(),
+    position_y: real(),
+  },
+  (table) => [
+    foreignKey({
+      columns: [table.design_id],
+      foreignColumns: [designs.id],
+      name: "fk_text_element_design",
+    }).onDelete("cascade"),
+  ]
+);
+
+export type InsertTextElement = typeof textElements.$inferInsert;
+export type SelectTextElement = typeof textElements.$inferSelect;
+
 export const messages = pgTable(
   "messages",
   {
