76413ff22a2e8f7b133e492206d537960477bc9a
Leif Etter <etter.leif@gmail.com>
2025-04-22 13:31:07 +0200

    add route for uploading single image to image router

--- src/components/image/image.route.ts
+++ src/components/image/image.route.ts
@@ -1,5 +1,14 @@
 import { Router } from "express";
+import { saveImage } from "./image.controller";
+import multer from "multer";
+import { minPower } from "auth/authorization";
+import { authenticate } from "auth/authentication";
 
-const router = Router();
+const multerClient = multer();
+const router = Router({ mergeParams: true });
+
+router
+  .route("/")
+  .post(authenticate, minPower(1), multerClient.single("image"), saveImage);
 
 export default router;
