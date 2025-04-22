166b1c9359d627aa092bf406b896ce91f02bb5e6
Leif Etter <etter.leif@gmail.com>
2025-04-22 13:37:40 +0200

    improve image controller, by adding a separate function for getting images

--- src/components/image/image.controller.ts
+++ src/components/image/image.controller.ts
@@ -7,43 +7,48 @@ import { images } from "db/schema";
 import { logger } from "logging/logger";
 import { eq } from "drizzle-orm";
 
+const insertImageIntoDb = async (userId: number): Promise<{ id: number }[]> =>
+  await db
+    .insert(images)
+    .values({ user_id: userId })
+    .returning({ id: images.id });
+
 export const saveImage = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const file: Express.Multer.File | undefined = req.file;
-  const userId: number = res.locals.user!.user_id;
-  const orderId: number = res.locals.params!.id!;
-  if (file == undefined) {
-    return next(new ApiError({ code: 400, info: errorMessages.missingImage }));
+  try {
+    const file: Express.Multer.File | undefined = req.file;
+
+    if (file == undefined) {
+      return next(
+        new ApiError({ code: 400, info: errorMessages.missingImage })
+      );
+    }
+    const userId: number = res.locals.user!.user_id;
+    const insertImageResult = await insertImageIntoDb(userId);
+    if (insertImageResult.length == 0) {
+      return next(
+        new ApiError({ code: 500, info: errorMessages.imageUploadFailed })
+      );
+    }
+    const uploadResult = await uploadImageToHetzner({
+      file: file.buffer,
+      path: `${process.env.NODE_ENV}/users/${userId}`,
+      filename: `${insertImageResult[0]?.id}`,
+      imageType: "image/png",
+    });
+    if (!uploadResult) {
+      await db.delete(images).where(eq(images.id, insertImageResult[0]!.id!));
+      return next(
+        new ApiError({ code: 400, info: errorMessages.imageUploadFailed })
+      );
+    }
+    res.status(200).send({
+      link: `${process.env.HETZNER_STORAGE_WITH_BUCKET}/${process.env.NODE_ENV}/users/${userId}/${insertImageResult[0]?.id}`,
+    });
+  } catch (error) {
+    next(error);
   }
-  const buffer: Buffer = file.buffer;
-  const imageRecord = await db
-    .insert(images)
-    .values({
-      user_id: userId,
-      order_id: orderId,
-    })
-    .returning({ id: images.id });
-  if (imageRecord.length == 0) {
-    return next(
-      new ApiError({ code: 500, info: errorMessages.imageUploadFailed })
-    );
-  }
-  const uploadResult = await uploadImageToHetzner({
-    file: buffer,
-    path: `${process.env.NODE_ENV}/users/${userId}`,
-    filename: `${imageRecord[0]?.id}`,
-    imageType: "image/png",
-  });
-  if (!uploadResult) {
-    await db.delete(images).where(eq(images.id, imageRecord[0]!.id!));
-    return next(
-      new ApiError({ code: 400, info: errorMessages.imageUploadFailed })
-    );
-  }
-  res.status(200).send({
-    link: `https://abipulli.nbg1.your-objectstorage.com/${process.env.NODE_ENV}/users/${userId}/${imageRecord[0]?.id}`,
-  });
 };
