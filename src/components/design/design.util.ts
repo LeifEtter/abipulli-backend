3672ec6ecd11b1ff6500a29c07ddeedc838750b7
Leif Etter <etter.leif@gmail.com>
2025-04-22 13:39:09 +0200

    create function for getting design by id

---
+++ src/components/design/design.util.ts
@@ -0,0 +1,10 @@
+import db from "db/db";
+import { designs, SelectDesign, users } from "db/schema";
+import { eq } from "drizzle-orm";
+
+export const getDesignById = async (
+  designId: number
+): Promise<SelectDesign | undefined> =>
+  await db.query.designs.findFirst({
+    where: eq(designs.id, designId),
+  });
