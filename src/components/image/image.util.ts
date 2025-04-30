b27953d1fa9604c79e812ec727eda8115a37ea63
Leif Etter <etter.leif@gmail.com>
2025-04-30 16:50:44 +0200

    add utility for using prompt to generate ideogram images and save these in hetzner storage

--- src/components/image/image.util.ts
+++ src/components/image/image.util.ts
@@ -109,3 +109,39 @@ export const requestImprovedPrompt = async (
   return { prompt: improvedPrompt, cost };
 };
 
+export const queryImageFromIdeogram = async (
+  prompt: string
+): Promise<string> => {
+  const res = await fetch(process.env.IDEOGRAM_URL!, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Api-Key": process.env.IDEOGRAM_KEY!,
+    },
+    body: JSON.stringify({
+      image_request: {
+        prompt: prompt,
+        aspect_ratio: "ASPECT_1_1",
+        model: process.env.IDEOGRAM_MODEL,
+        magic_prompt_option: "AUTO",
+      },
+    }),
+  });
+  if (!res.ok) {
+    logger.error(await res.text());
+    throw ApiError.internal({
+      msg: "Ideogram Query failed",
+      code: 50,
+    });
+  }
+  const body = await res.json();
+  const imageUrl: string | undefined = body.data[0].url;
+  if (imageUrl == undefined) {
+    throw ApiError.internal({
+      msg: "Couldn't find Image Url in Ideogram Response",
+      code: 50,
+    });
+  }
+  return imageUrl;
+};
+
