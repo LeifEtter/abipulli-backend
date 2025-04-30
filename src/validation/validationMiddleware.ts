a48e1fa9a6cc5620c28606da871e9b9c3b884842
Leif Etter <etter.leif@gmail.com>
2025-04-30 14:00:35 +0200

    properly extract issues from zod validation result and return to user

--- src/validation/validationMiddleware.ts
+++ src/validation/validationMiddleware.ts
@@ -1,20 +1,30 @@
 import type { NextFunction, Request, Response } from "express";
-import { z, ZodError } from "zod";
+import { z, ZodError, ZodIssue } from "zod";
 import { logger } from "../logging/logger";
 import ApiError from "error/ApiError";
 import { errorMessages } from "error/errorMessages";
 
+export const extractIssues = (issues: ZodIssue[]): string[] =>
+  issues.map(
+    (issue: ZodIssue) => " " + (issue.path[0] ?? "unknown").toString()
+  );
+
+//TODO: Implement Correct Error Message on Faulty Value for a specific key, e.g. string instead of array of strings
 export const validateBody =
   (schema: z.ZodObject<any, any>) =>
   (req: Request, res: Response, next: NextFunction) => {
     try {
       const result = schema.safeParse(req.body);
       if (!result.success) {
-        res.status(400).json("Error during Validation.");
+        const issues: string[] = extractIssues(result.error.issues);
+        res.status(400).send({
+          err_code: 16,
+          err_msg: `Deinem Request Body fehlen folgende/r Key:${issues}`,
+        });
+      } else {
+        req.body = result.data;
+        next();
       }
-
-      req.body = result.data;
-      return next();
     } catch (err) {
       if (err instanceof ZodError) {
         logger.error(err);
