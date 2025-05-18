07869c0150cc1a4a7cd6162b7f3514bc7054f787
Leif Etter <etter.leif@gmail.com>
2025-05-18 17:09:40 +0200

    add and implement cors

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,25 @@ import { httpLogger } from "./logging/logger";
 import router from "./route";
 import { apiErrorHandler } from "./error/errorMiddleware";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
 
 app.use(httpLogger);
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://www.postman.com",
+      "http://localhost",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(router);
 
 app.use(apiErrorHandler);
