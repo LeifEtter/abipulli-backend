50e06307272e94dfaa218ac56e54a8f053d33bf3
Leif Etter <etter.leif@gmail.com>
2025-05-22 14:48:03 +0200

    implement shared types for error response into errorMiddleware

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,7 +1,9 @@
 import type { NextFunction, Request, Response } from "express";
 import { logger } from "../lib/logger";
 import { ApiError } from "error/ApiError";
+import { ApiResponse, ErrorResponse } from "abipulli-types";
 
+// ! NOTE TO SELF: NEVER REMOVE ANY PARAMS OTHERWISE ERROR HANDLER WILL BE CALLED TOO LATE
 export function apiErrorHandler(
   err: Error,
   req: Request,
@@ -10,13 +12,22 @@ export function apiErrorHandler(
 ) {
   logger.error(err);
   console.error(err);
+
   if (err instanceof ApiError) {
-    res.status(err.code).json({
-      err_code: err.code,
-      err_msg: err.message,
-      err_resource: err.resource,
-    });
+    const errorResponse: ErrorResponse = {
+      success: false,
+      error: {
+        code: err.code,
+        info: err.info,
+        resource: err.resource,
+      },
+    };
+    res.status(err.code).json(errorResponse);
     return;
   }
-  res.status(500).json("Uh Oh, Something went Wrong (╥﹏╥) ");
+  const internalErrorResponse: ErrorResponse = {
+    success: false,
+    error: { code: 500, info: "Uh Oh, an unknown Error occured (╥﹏╥)" },
+  };
+  res.status(500).json(internalErrorResponse);
 }
