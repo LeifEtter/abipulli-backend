9ff6c1643ba5b364390ae1b5ca37fd1616934ed6
Leif Etter <etter.leif@gmail.com>
2025-05-24 17:25:31 +0200

    remove origin key from image db

--- src/services/images/castImage.service.ts
+++ src/services/images/castImage.service.ts
@@ -5,7 +5,6 @@ export const castImage = (image: SelectImage): Image => {
   return {
     id: image.id,
     createdAt: new Date(image.created_at),
-    origin: image.origin ?? "",
     generated: image.generated ?? false,
     prompt: image.prompt ?? "",
     userId: image.user_id ?? 0,
