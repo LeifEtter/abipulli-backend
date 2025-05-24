863a70b9bbbcbc16285b7e41e5ecae85dd70ee0a
Leif Etter <etter.leif@gmail.com>
2025-05-24 16:53:22 +0200

    make vitest config ts and add test env settings

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,4 +3,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    include: ["vitest/**/*.test.ts"],
+    environment: "node",
+  },
 });
