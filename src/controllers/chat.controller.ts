a85553d2757e4c13ebcd7a4e19fd370f7a75882f
Leif Etter <etter.leif@gmail.com>
2025-05-29 17:55:42 +0200

    add controller for getting chat

--- src/controllers/chat.controller.ts
+++ src/controllers/chat.controller.ts
@@ -1,7 +1,8 @@
-import { ChatResponse, ChatSchema } from "abipulli-types";
+import { Chat, ChatResponse, errorMessages } from "abipulli-types";
 import { NextFunction, Request, Response } from "express";
 import { ApiError } from "src/error/ApiError";
 import { createChat } from "src/services/chats/createChat.service";
+import { getChatWithMessagesFromDb } from "src/services/chats/getChat.service";
 
 export const createChatController = async (
   req: Request,
@@ -19,6 +20,44 @@ export const createChatController = async (
     const chatResponse: ChatResponse = { success: true, data: createdChat };
     res.status(201).send(chatResponse);
   } catch (error) {
-    next(ApiError.internal({ errorInfo: null }));
+    next(error);
+  }
+};
+
+export const getChatController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const chatId: number = res.locals.params.chatId!;
+    const userId: number = res.locals.user.user_id;
+    const chat: Chat | undefined = await getChatWithMessagesFromDb(chatId);
+
+    if (!chat) {
+      return next(
+        new ApiError({
+          code: 404,
+          info: errorMessages.resourceNotFound,
+          resource: "Chat",
+        })
+      );
+    }
+    if (chat?.userId != userId) {
+      return next(
+        new ApiError({
+          code: 401,
+          info: errorMessages.resourceNotOwned,
+          resource: "Chat",
+        })
+      );
+    }
+    const chatResponse: ChatResponse = {
+      data: chat,
+      success: true,
+    };
+    res.status(200).send(chatResponse);
+  } catch (error) {
+    next(error);
   }
 };
