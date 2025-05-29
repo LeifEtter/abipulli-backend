357290dbec6c148bd655efa1d7aa4490bf6c8ded
Leif Etter <etter.leif@gmail.com>
2025-05-29 23:38:20 +0200

    add route for getting chat with messages

--- src/routes/chat.route.ts
+++ src/routes/chat.route.ts
@@ -1,8 +1,16 @@
+import { ChatCreateSchema } from "abipulli-types";
 import { Router } from "express";
-import { createChatController } from "src/controllers/chat.controller";
+import {
+  createChatController,
+  getChatController,
+} from "src/controllers/chat.controller";
 import { authenticateHttp } from "src/middleware/authentication.middleware";
 import { minPower } from "src/middleware/authorization.middleware";
-import { validateParams } from "src/middleware/validation.middleware";
+import {
+  validateBody,
+  validateParams,
+} from "src/middleware/validation.middleware";
+
 const router = Router({ mergeParams: true });
 
 router
@@ -12,6 +20,16 @@ router
     minPower(1),
     validateParams({ requiredParams: ["orderId"] }),
     createChatController
+  );
+
+router
+  .route("/:chatId")
+  .get(
+    authenticateHttp,
+    minPower(1),
+    validateParams({ requiredParams: ["chatId"] }),
+    validateBody(ChatCreateSchema),
+    getChatController
   );
 
 export default router;
