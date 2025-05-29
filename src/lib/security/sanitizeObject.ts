98224a00e9d3828367e6bc6ee45dc15ed5f359ad
Leif Etter <etter.leif@gmail.com>
2025-05-29 22:56:56 +0200

    create recurvise function that sanitizes a given element (array, object, string)

---
+++ src/lib/security/sanitizeObject.ts
@@ -0,0 +1,24 @@
+import sanitize from "sanitize-html";
+
+export const sanitizeElement = (e: any): any => {
+  if (typeof e == "string") {
+    const sanitized = sanitize(e);
+    return sanitized;
+  } else if (Array.isArray(e)) {
+    let sanitizedArray = [];
+    for (let listElement of e) {
+      sanitizedArray.push(sanitizeElement(listElement));
+    }
+    return sanitizedArray;
+  } else if (typeof e == "object") {
+    let sanitizedObject = {};
+    for (let currentKey of Object.keys(e as object)) {
+      console.log(currentKey);
+      const newValue = sanitizeElement(e[currentKey]);
+      sanitizedObject = { ...sanitizedObject, [currentKey]: newValue };
+    }
+    return sanitizedObject;
+  } else {
+    return e;
+  }
+};
