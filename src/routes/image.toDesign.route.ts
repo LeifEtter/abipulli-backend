81ed88f22d65580c9e1968dea7205f72f231246a
Leif Etter <etter.leif@gmail.com>
2025-06-07 14:31:36 +0200

    add route for getting all images for a design

--- src/routes/image.toDesign.route.ts
+++ src/routes/image.toDesign.route.ts
@@ -1,10 +1,29 @@
-import { placeImageOnDesignController } from "src/controllers/image.toDesignController";
+import {
+  getAllImagesForDesignController,
+  manipulateImageController,
+  placeImageOnDesignController,
+} from "src/controllers/image.toDesignController";
 import { Router } from "express";
 import { authenticateHttp } from "src/middleware/authentication.middleware";
 import { minPower } from "src/middleware/authorization.middleware";
-import { validateBody } from "src/middleware/validation.middleware";
-import { AddImageToDesignParamsSchema } from "abipulli-types";
+import {
+  validateBody,
+  validateParams,
+} from "src/middleware/validation.middleware";
+import {
+  AddImageToDesignParamsSchema,
+  ManipulateImageInDesignParamsSchema,
+} from "abipulli-types";
 const router = Router({ mergeParams: true });
+
+router
+  .route("/")
+  .get(
+    authenticateHttp,
+    minPower(1),
+    validateParams({ requiredParams: ["designId"] }),
+    getAllImagesForDesignController
+  );
 
 router
   .route("/")
