6762f337fb34ef5ce7bed5f7ae1c50d5cad172d0
Leif Etter <etter.leif@gmail.com>
2025-06-07 14:30:43 +0200

    add controller for getting all images for a design

--- src/controllers/image.toDesignController.ts
+++ src/controllers/image.toDesignController.ts
@@ -2,13 +2,17 @@ import {
   AddImageToDesignParams,
   ApiResponse,
   Design,
-  errorMessages,
+  ImagesForDesignResponse,
+  ImageWithPositionAndScale,
   ManipulateImageInDesignParams,
 } from "abipulli-types";
 import { ApiError } from "src/error/ApiError";
 import { NextFunction, Request, Response } from "express";
 import { logger } from "src/lib/logger";
-import { getImageById } from "src/services/images/getImageById.service";
+import {
+  getImageById,
+  getImagesByDesignId,
+} from "src/services/images/getImageById.service";
 import { placeImageOnDesign } from "src/services/images/placeImage.service";
 import { getDesignById } from "src/services/designs/getDesigns.service";
 import { manipulateImageOnDesign } from "src/services/images/manipulateImage.service";
@@ -69,6 +73,37 @@ export const manipulateImageController = async (
     };
     res.status(200).send(response);
   } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllImagesForDesignController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const userId: number = res.locals.user.user_id;
+    const designId: number = res.locals.params.designId!;
+    const design = await getDesignById(designId);
+    if (!design) return next(ApiError.notFound({ resource: "Design" }));
+    // if (design?.customerId != userId)
+    //   return next(ApiError.notOwned({ resource: "Design" }));
+    const images: ImageWithPositionAndScale[] = await getImagesByDesignId(
+      designId
+    );
+    const imageResponse: ImagesForDesignResponse = {
+      success: true,
+      data: {
+        items: images,
+        total: images.length,
+        page: 1,
+        pageSize: images.length,
+      },
+    };
+    res.status(200).send(imageResponse);
+  } catch (error) {
+    console.log(error);
     next(error);
   }
 };
