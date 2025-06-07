3bd69c447994cdc7182b3a878274c413289dca04
Leif Etter <etter.leif@gmail.com>
2025-06-07 14:27:15 +0200

    add service for getting all images given a design id

--- src/services/images/getImageById.service.ts
+++ src/services/images/getImageById.service.ts
@@ -1,8 +1,23 @@
-import { images, SelectImage } from "src/db";
+import { images, imageToDesign, SelectImage } from "src/db";
 import db from "src/db/db";
 import { eq } from "drizzle-orm";
+import { ImageWithPositionAndScale } from "abipulli-types";
+import { castImage, castImageWithPositionAndScale } from "./castImage.service";
 
 export const getImageById = async (
   imageId: number
 ): Promise<SelectImage | undefined> =>
   await db.query.images.findFirst({ where: eq(images.id, imageId) });
+
+export const getImagesByDesignId = async (
+  designId: number
+): Promise<ImageWithPositionAndScale[]> => {
+  const dbImages = await db.query.imageToDesign.findMany({
+    where: eq(imageToDesign.design_id, designId),
+    with: { image: true },
+  });
+  const images: ImageWithPositionAndScale[] = dbImages.map((e) =>
+    castImageWithPositionAndScale(e)
+  );
+  return images;
+};
