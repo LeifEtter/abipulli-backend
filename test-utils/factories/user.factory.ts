8c179b751b8dc7e5594078fde4d4e99d3797a86b
Leif Etter <etter.leif@gmail.com>
2025-06-09 23:47:34 +0200

    update user factory to properly return user details

--- test-utils/factories/user.factory.ts
+++ test-utils/factories/user.factory.ts
@@ -3,24 +3,29 @@ import { users } from "src/db";
 import db from "src/db/db";
 
 export interface UserFactory {
-  insertSingleUser: () => Promise<number>;
+  insertSingleUser: () => Promise<{
+    email: string;
+    password: string;
+    userId: number;
+  }>;
 }
 
-export const insertSingleUser = async (): Promise<number> => {
+export const insertSingleUser = async () => {
+  const email: string = fakerDE.internet.email();
+  const password: string = fakerDE.internet.password() + "@123";
   const insertedUserId = await db
     .insert(users)
     .values({
       first_name: fakerDE.person.firstName(),
       last_name: fakerDE.person.lastName(),
-      email: fakerDE.internet.email(),
-      password: fakerDE.internet.password(),
+      email,
+      password,
       verified: true,
       school: fakerDE.location.street() + "Gymnasium",
       role_id: 1,
     })
     .returning({ id: users.id });
-
-  return insertedUserId[0]!.id!;
+  return { userId: insertedUserId[0]!.id!, email, password };
 };
 
 export const userFactory: UserFactory = {
