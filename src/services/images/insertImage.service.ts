b31a997bc67cf63cebc2ced8264b63a836dffd64
Leif Etter <etter.leif@gmail.com>
2025-06-09 23:48:51 +0200

    update image insert service to include width and height

--- src/services/images/insertImage.service.ts
+++ src/services/images/insertImage.service.ts
@@ -2,14 +2,30 @@ import { images } from "src/db";
 import db from "src/db/db";
 import { ApiError } from "src/error/ApiError";
 
-export const insertImageIntoDb = async (
-  userId: number,
-  fileSize: number,
-  fileUuid: string
-): Promise<number> => {
+interface InsertImageIntoDBParams {
+  userId: number;
+  fileSize: number;
+  fileUuid: string;
+  width: number;
+  height: number;
+}
+
+export const insertImageIntoDb = async ({
+  userId,
+  fileSize,
+  fileUuid,
+  width,
+  height,
+}: InsertImageIntoDBParams): Promise<number> => {
   const result = await db
     .insert(images)
-    .values({ user_id: userId, file_size: fileSize, file_uuid: fileUuid })
+    .values({
+      user_id: userId,
+      file_size: fileSize,
+      file_uuid: fileUuid,
+      image_width: width,
+      image_height: height,
+    })
     .returning({ id: images.id });
   if (result[0] == null) {
     throw ApiError.internal({
