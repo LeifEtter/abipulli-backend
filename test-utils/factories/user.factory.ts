1bc5c382cff9372b6377094f9f8ad3254a78cb65
Leif Etter <etter.leif@gmail.com>
2025-06-10 23:20:27 +0200

    fix bug where during user gen the plain password was saved in db and use correct role

--- test-utils/factories/user.factory.ts
+++ test-utils/factories/user.factory.ts
@@ -1,6 +1,8 @@
 import { fakerDE } from "@faker-js/faker";
 import { users } from "src/db";
 import db from "src/db/db";
+import bcrypt from "bcrypt";
+import { SALT_ROUNDS } from "src/constants";
 
 export interface UserFactory {
   insertSingleUser: () => Promise<{
@@ -13,16 +15,17 @@ export interface UserFactory {
 export const insertSingleUser = async () => {
   const email: string = fakerDE.internet.email();
   const password: string = fakerDE.internet.password() + "@123";
+  const encryptedPassword: string = await bcrypt.hash(password, SALT_ROUNDS);
   const insertedUserId = await db
     .insert(users)
     .values({
       first_name: fakerDE.person.firstName(),
       last_name: fakerDE.person.lastName(),
       email,
-      password,
+      password: encryptedPassword,
       verified: true,
       school: fakerDE.location.street() + "Gymnasium",
-      role_id: 1,
+      role_id: 2,
     })
     .returning({ id: users.id });
   return { userId: insertedUserId[0]!.id!, email, password };
