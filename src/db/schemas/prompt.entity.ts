da37d427a1d4b416221fff097b3c56264ee5e857
Leif Etter <etter.leif@gmail.com>
2025-06-13 23:59:58 +0200

    zpdate prompt entity to include user id

--- src/db/schemas/prompt.entity.ts
+++ src/db/schemas/prompt.entity.ts
@@ -1,16 +1,35 @@
-import { pgTable, serial, timestamp, varchar } from "drizzle-orm/pg-core";
+import {
+  foreignKey,
+  integer,
+  pgTable,
+  serial,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+import { users } from "./user.entity";
 
-export const prompts = pgTable("prompts", {
-  id: serial().notNull().primaryKey(),
-  title: varchar().notNull(),
-  description: varchar().notNull(),
-  content: varchar().notNull(),
-  purpose: varchar(),
-  created_at: timestamp().notNull().defaultNow(),
-  updated_at: timestamp()
-    .notNull()
-    .$onUpdate(() => new Date()),
-});
+export const prompts = pgTable(
+  "prompts",
+  {
+    id: serial().notNull().primaryKey(),
+    user_id: integer().notNull(),
+    title: varchar().notNull(),
+    description: varchar().notNull(),
+    content: varchar().notNull(),
+    purpose: varchar(),
+    created_at: timestamp().notNull().defaultNow(),
+    updated_at: timestamp()
+      .notNull()
+      .$onUpdate(() => new Date()),
+  },
+  (table) => [
+    foreignKey({
+      columns: [table.user_id],
+      foreignColumns: [users.id],
+      name: "fk_prompt_user_id",
+    }).onDelete("cascade"),
+  ]
+);
 
 export type InsertPrompt = typeof prompts.$inferInsert;
 export type SelectPrompt = typeof prompts.$inferSelect;
