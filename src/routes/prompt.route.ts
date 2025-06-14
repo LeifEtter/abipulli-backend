4631a5cfae744c36cf66a315de22dfda5590636f
Leif Etter <etter.leif@gmail.com>
2025-06-14 09:38:57 +0200

    add routes for getting all prompts for single user and creating single prompt

--- src/routes/prompt.route.ts
+++ src/routes/prompt.route.ts
@@ -1,5 +1,17 @@
 import { Router } from "express";
+import {
+  getAllPromptsByUserController,
+  insertPromptController,
+} from "src/controllers/prompt.controller";
+import { authenticateHttp } from "src/middleware/authentication.middleware";
+import { minPower } from "src/middleware/authorization.middleware";
 
 const router: Router = Router();
+
+router
+  .route("/me")
+  .get(authenticateHttp, minPower(1), getAllPromptsByUserController);
+
+router.route("/").post(authenticateHttp, minPower(1), insertPromptController);
 
 export default router;
