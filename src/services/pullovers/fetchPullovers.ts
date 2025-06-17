78a4fbe75385efb72e8697ce200fdb430d2b5fcd
Leif Etter <etter.leif@gmail.com>
2025-06-17 22:55:03 +0200

    add service for fetching all pullovers including images

---
+++ src/services/pullovers/fetchPullovers.ts
@@ -0,0 +1,15 @@
+import { Pullover } from "abipulli-types";
+import db from "src/db/db";
+import { castPullover } from "./castPullover.service";
+import { SelectPulloverWithImage } from "src/db";
+
+export const fetchAllPullovers = async (): Promise<Pullover[]> => {
+  const dbPullovers: SelectPulloverWithImage[] =
+    await db.query.pullovers.findMany({
+      with: { image: true },
+    });
+  const castedPullovers: Pullover[] = dbPullovers.map((pullover) =>
+    castPullover(pullover)
+  );
+  return castedPullovers;
+};
