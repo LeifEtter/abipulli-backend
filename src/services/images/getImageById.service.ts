e9121eb89cda3df76cae8d62fa777b188e3faad3
Leif Etter <etter.leif@gmail.com>
2025-06-25 22:17:27 +0200

    configure getImageById function to return castedImage instead of db image

--- src/services/images/getImageById.service.ts
+++ src/services/images/getImageById.service.ts
@@ -7,8 +7,13 @@ import { ApiError } from "src/error/ApiError";
 
 export const getImageById = async (
   imageId: number
-): Promise<SelectImage | undefined> =>
-  await db.query.images.findFirst({ where: eq(images.id, imageId) });
+): Promise<Image | undefined> => {
+  const dbImage: SelectImage | undefined = await db.query.images.findFirst({
+    where: eq(images.id, imageId),
+  });
+  if (!dbImage) return undefined;
+  return castImage(dbImage);
+};
 
 export const getImageWithPositionAndScale = async (
   imageToDesignId: number
