c257cb145767e50ca719a3bfd3e30cd3e880175a
Leif Etter <etter.leif@gmail.com>
2025-06-26 14:55:09 +0200

    extract some env variables into config files

---
+++ src/configs/hetzner.config.ts
@@ -0,0 +1,3 @@
+export const HETZNER_STORAGE: string = "https://nbg1.your-objectstorage.com";
+export const HETZNER_STORAGE_WITH_BUCKET: string =
+  "https://abipulli.nbg1.your-objectstorage.com";
---
+++ src/configs/openai.config.ts
@@ -0,0 +1,2 @@
+export const OPENAI_URL: string = "https://api.openai.com/v1/responses";
+export const OPENAPI_MODEL: string = "gpt-4o";
