74c456362835b0b59b00d80363a7170c296bc467
Leif Etter <etter.leif@gmail.com>
2025-06-30 23:55:35 +0200

    modify type of db return object to include client to enable .end()

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,25 +1,27 @@
 import "dotenv/config";
 import { drizzle, NodePgDatabase } from "drizzle-orm/node-postgres";
 import * as entities from "./index";
-import pg from "pg";
+import pg, { Pool as PL } from "pg";
 const { Pool } = pg;
 
 export const schema = {
   ...entities,
 };
 
-let dbInstance: NodePgDatabase<typeof schema>;
+let dbInstance: NodePgDatabase<typeof schema> & { $client: PL };
 
 export const initDb = (poolOrUrl: string | pg.Pool) => {
   const pool: pg.Pool =
     typeof poolOrUrl === "string"
       ? new Pool({ connectionString: poolOrUrl })
       : poolOrUrl;
-  dbInstance = drizzle(pool, { schema: { ...entities } });
+  dbInstance = drizzle(pool, {
+    schema: { ...entities },
+  });
   return dbInstance;
 };
 
-export const getDb = (): NodePgDatabase<typeof schema> => {
+export const getDb = (): NodePgDatabase<typeof schema> & { $client: PL } => {
   if (!dbInstance) throw new Error("Drizzle DB not initialized. Call initDb()");
   return dbInstance;
 };
