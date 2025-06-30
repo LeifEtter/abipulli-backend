dddbcb9d22a47c13a25b57851f50601f7d34fa11
Leif Etter <etter.leif@gmail.com>
2025-06-30 13:59:21 +0200

    convert db.ts to support injected db for testing purposes

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,10 +1,25 @@
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle, NodePgDatabase } from "drizzle-orm/node-postgres";
 import * as entities from "./index";
 import pg from "pg";
 const { Pool } = pg;
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const db = drizzle(pool, { schema: { ...entities } });
+export const schema = {
+  ...entities,
+};
 
-export default db;
+let dbInstance: NodePgDatabase<typeof schema>;
+
+export const initDb = (poolOrUrl: string | pg.Pool) => {
+  const pool: pg.Pool =
+    typeof poolOrUrl === "string"
+      ? new Pool({ connectionString: poolOrUrl })
+      : poolOrUrl;
+  dbInstance = drizzle(pool, { schema: { ...entities } });
+  return dbInstance;
+};
+
+export const getDb = (): NodePgDatabase<typeof schema> => {
+  if (!dbInstance) throw new Error("Drizzle DB not initialized. Call initDb()");
+  return dbInstance;
+};
