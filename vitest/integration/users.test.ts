32534169d62341d4de0fb45705ee52ccbac13d5d
Leif Etter <etter.leif@gmail.com>
2025-07-05 23:46:17 +0200

    add test for castUser

--- vitest/integration/users.test.ts
+++ vitest/integration/users.test.ts
@@ -1,11 +1,23 @@
 import { StartedPostgreSqlContainer } from "@testcontainers/postgresql";
 import { getDb } from "src/db/db";
-import { afterAll, beforeAll, describe, expect, it } from "vitest";
-import { users } from "src/db";
+import {
+  afterAll,
+  afterEach,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+} from "vitest";
+import { InsertUser, SelectUser, users } from "src/db";
 import { getUserById } from "src/services/users/getUser.service";
-import { User } from "abipulli-types";
+import { User, UserSchema } from "abipulli-types";
 import insertRoles from "src/db/seed/insert-roles";
 import { setupFakeDb, teardownFakeDb } from "vitest/helpers/fake.db";
+import { userFactory } from "vitest/test-utils/factories/user.factory";
+import { castUser } from "src/services/users/castUser.service";
+import { eq } from "drizzle-orm";
+import { PoolClient } from "pg";
 
 let container: StartedPostgreSqlContainer;
 
@@ -30,7 +42,30 @@ afterEach(async () => {
 });
 
 describe("User Service Integration", () => {
-  describe("getUser()", async () => {
+  describe("castUser", async () => {
+    it("should return a casted user", async () => {
+      const insertedUser: InsertUser = await userFactory.insertSingleUser();
+      const retrievedUser: SelectUser | undefined =
+        await getDb().query.users.findFirst({
+          where: eq(users.id, insertedUser.id!),
+          with: {
+            role: true,
+          },
+        });
+      const castedUser = castUser(retrievedUser!);
+      expect(castedUser).toMatchObject({
+        id: insertedUser.id,
+        email: insertedUser.email,
+        password: insertedUser.password,
+        role: {
+          id: insertedUser.role_id,
+        },
+      });
+      expect(UserSchema.safeParse(castedUser).success).toEqual(true);
+    });
+  });
+
+  describe("getUser", async () => {
     it("returns correct user by id", async () => {
       const db = getDb();
       await insertRoles();
