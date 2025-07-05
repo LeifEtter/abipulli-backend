c498bf7dd217ffb8cb096e2b6722a47912f07321
Leif Etter <etter.leif@gmail.com>
2025-07-05 23:47:36 +0200

    fix some funcitons in user factory

--- vitest/test-utils/factories/user.factory.ts
+++ vitest/test-utils/factories/user.factory.ts
@@ -1,21 +1,17 @@
 import { fakerDE } from "@faker-js/faker";
 import bcrypt from "bcrypt";
 import { SALT_ROUNDS } from "src/constants";
-import { users } from "src/db";
+import { InsertUser, users } from "src/db";
 import { getDb } from "src/db/db";
 export interface UserFactory {
-  insertSingleUser: () => Promise<{
-    email: string;
-    password: string;
-    userId: number;
-  }>;
+  insertSingleUser: () => Promise<InsertUser>;
 }
 
 export const insertSingleUser = async () => {
   const email: string = fakerDE.internet.email();
   const password: string = fakerDE.internet.password() + "@123";
   const encryptedPassword: string = await bcrypt.hash(password, SALT_ROUNDS);
-  const insertedUserId = await getDb()
+  const insertedUsers: InsertUser[] = await getDb()
     .insert(users)
     .values({
       first_name: fakerDE.person.firstName(),
@@ -26,8 +22,8 @@ export const insertSingleUser = async () => {
       school: fakerDE.location.street() + "Gymnasium",
       role_id: 2,
     })
-    .returning({ id: users.id });
-  return { userId: insertedUserId[0]!.id!, email, password };
+    .returning();
+  return insertedUsers[0]!;
 };
 
 export const userFactory: UserFactory = {
