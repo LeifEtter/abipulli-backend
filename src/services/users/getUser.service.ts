5e947c0354063e1d1ad1e3b16fcb5b033387a303
Leif Etter <etter.leif@gmail.com>
2025-07-14 14:12:44 +0200

    add function for getting user id with password by id

--- src/services/users/getUser.service.ts
+++ src/services/users/getUser.service.ts
@@ -13,7 +13,17 @@ export const getUserWithPasswordByEmail = async (
     with: { role: true },
   });
 
-export const getUserById = async (id: number): Promise<User | undefined> => {
+export const getUserWithPasswordById = async (
+  id: number
+): Promise<SelectUser | undefined> =>
+  await getDb().query.users.findFirst({
+    where: eq(users.id, id),
+    with: { role: true },
+  });
+
+export const getUserById = async (
+  id: number
+): Promise<Omit<User, "password"> | undefined> => {
   const dbUser = await getDb().query.users.findFirst({
     where: eq(users.id, id),
     columns: { password: false },
