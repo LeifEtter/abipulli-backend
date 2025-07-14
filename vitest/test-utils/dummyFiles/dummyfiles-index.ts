81ff52782eaafd91143153fbd0f49ad8a2d9fd09
Leif Etter <etter.leif@gmail.com>
2025-07-14 14:12:19 +0200

    update paths in dummyfiles index

--- vitest/test-utils/dummyFiles/dummyfiles-index.ts
+++ vitest/test-utils/dummyFiles/dummyfiles-index.ts
@@ -4,48 +4,48 @@ import path from "path";
 
 // Abicetamol
 const abicetamol1: Buffer = fs.readFileSync(
-  path.join("test-utils/dummyFiles/Abicetamol-1.jpeg")
+  path.join("vitest/test-utils/dummyFiles/Abicetamol-1.jpeg")
 );
 
 // Abikropolis
 const abikropolis1: Buffer = fs.readFileSync(
-  path.join("test-utils/dummyFiles/Abikropolis-1.png")
+  path.join("vitest/test-utils/dummyFiles/Abikropolis-1.png")
 );
 const abikropolis2: Buffer = fs.readFileSync(
-  path.join("test-utils/dummyFiles/Abikorpolis-2.jpeg")
+  path.join("vitest/test-utils/dummyFiles/Abikorpolis-2.jpeg")
 );
 
 // Abinsbett
 const abinsbett1: Buffer = fs.readFileSync(
-  path.join("test-utils/dummyFiles/Abinsbett-1.png")
+  path.join("vitest/test-utils/dummyFiles/Abinsbett-1.png")
 );
 const abinsbett2: Buffer = fs.readFileSync(
-  path.join("test-utils/dummyFiles/Abinsbett-2.png")
+  path.join("vitest/test-utils/dummyFiles/Abinsbett-2.png")
 );
 const abinsbett3: Buffer = fs.readFileSync(
-  path.join("test-utils/dummyFiles/Abinsbett-3.png")
+  path.join("vitest/test-utils/dummyFiles/Abinsbett-3.png")
 );
 const abinsbett4: Buffer = fs.readFileSync(
-  path.join("test-utils/dummyFiles/Abinsbett-4.png")
+  path.join("vitest/test-utils/dummyFiles/Abinsbett-4.png")
 );
 
 // Abirol
 const abirol1: Buffer = fs.readFileSync(
-  path.join("test-utils/dummyFiles/Abirol-1.jpeg")
+  path.join("vitest/test-utils/dummyFiles/Abirol-1.jpeg")
 );
 const abirol2: Buffer = fs.readFileSync(
-  path.join("test-utils/dummyFiles/Abirol-2.jpeg")
+  path.join("vitest/test-utils/dummyFiles/Abirol-2.jpeg")
 );
 const abirol3: Buffer = fs.readFileSync(
-  path.join("test-utils/dummyFiles/Abirol-3.jpeg")
+  path.join("vitest/test-utils/dummyFiles/Abirol-3.jpeg")
 );
 const abirol3Back: Buffer = fs.readFileSync(
-  path.join("test-utils/dummyFiles/Abirol-3-Back.png")
+  path.join("vitest/test-utils/dummyFiles/Abirol-3-Back.png")
 );
 
 // Abivegas
 const abivegas1: Buffer = fs.readFileSync(
-  path.join("test-utils/dummyFiles/Abivegas-1.png")
+  path.join("vitest/test-utils/dummyFiles/Abivegas-1.png")
 );
 
 const dummyImages = [
