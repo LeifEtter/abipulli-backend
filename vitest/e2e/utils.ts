6969a041547217af1a5b4f04e3b4f57d34e91e58
Leif Etter <etter.leif@gmail.com>
2025-08-12 16:55:20 +0200

    comment out test files for dev

--- vitest/e2e/utils.ts
+++ vitest/e2e/utils.ts
@@ -1,44 +1,44 @@
-import { faker } from "@faker-js/faker";
-import { eq } from "drizzle-orm";
-import { InsertOrder, InsertUser, orders, users } from "../../src/db";
-import { getDb } from "src/db/db";
+// import { faker } from "@faker-js/faker";
+// import { eq } from "drizzle-orm";
+// import { InsertOrder, InsertUser, orders, users } from "../../src/db";
+// import { getDb } from "src/db/db";
 
-export const testUtils = {
-  insertUser: async (user: InsertUser): Promise<number> =>
-    (await getDb().insert(users).values(user).returning())[0]!.id,
-  insertOrder: async (order: InsertOrder): Promise<number> =>
-    (await getDb().insert(orders).values(order).returning())[0]!.id,
-  deleteUser: async (id: number) =>
-    await getDb().delete(users).where(eq(users.id, id)),
-};
+// export const testUtils = {
+//   insertUser: async (user: InsertUser): Promise<number> =>
+//     (await getDb().insert(users).values(user).returning())[0]!.id,
+//   insertOrder: async (order: InsertOrder): Promise<number> =>
+//     (await getDb().insert(orders).values(order).returning())[0]!.id,
+//   deleteUser: async (id: number) =>
+//     await getDb().delete(users).where(eq(users.id, id)),
+// };
 
-export const mockUtils = {
-  user: ({ roleId }: { roleId: number }): InsertUser => ({
-    first_name: faker.person.fullName(),
-    last_name: faker.person.lastName(),
-    email: faker.internet.email(),
-    password: faker.internet.password({ length: 20 }),
-    role_id: roleId,
-  }),
-  order: ({ userId }: { userId: number }): InsertOrder => ({
-    user_id: userId,
-    deadline: faker.date.anytime(),
-    destination_country: faker.location.country(),
-    student_amount: faker.number.int({ min: 50, max: 150 }),
-    motto: faker.word.words(2),
-    school_name: faker.location.street() + "Gymnasium",
-    status: "pending",
-  }),
-};
+// export const mockUtils = {
+//   user: ({ roleId }: { roleId: number }): InsertUser => ({
+//     first_name: faker.person.fullName(),
+//     last_name: faker.person.lastName(),
+//     email: faker.internet.email(),
+//     password: faker.internet.password({ length: 20 }),
+//     role_id: roleId,
+//   }),
+//   order: ({ userId }: { userId: number }): InsertOrder => ({
+//     user_id: userId,
+//     deadline: faker.date.anytime(),
+//     destination_country: faker.location.country(),
+//     student_amount: faker.number.int({ min: 50, max: 150 }),
+//     motto: faker.word.words(2),
+//     school_name: faker.location.street() + "Gymnasium",
+//     status: "pending",
+//   }),
+// };
 
-const exampleOrder = {
-  user_id: 5,
-  deadline: "20/04/2024",
-  destination_country: "Deutschland",
-  student_amount: 83,
-  school_name: "Otto-Schott-Gymnasium",
-  motto: "AbIns Bett",
-};
+// const exampleOrder = {
+//   user_id: 5,
+//   deadline: "20/04/2024",
+//   destination_country: "Deutschland",
+//   student_amount: 83,
+//   school_name: "Otto-Schott-Gymnasium",
+//   motto: "AbIns Bett",
+// };
 
-export const tDeleteUser = async (email: string) =>
-  await getDb().delete(users).where(eq(users.email, email));
+// export const tDeleteUser = async (email: string) =>
+//   await getDb().delete(users).where(eq(users.email, email));
