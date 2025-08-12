16d669a78bfe0b4a9b6d6dcfdfb2f29133e85c09
Leif Etter <etter.leif@gmail.com>
2025-08-13 00:37:27 +0200

    uncomment and update tests

--- vitest/e2e/user.test.ts
+++ vitest/e2e/user.test.ts
@@ -1,65 +1,65 @@
-// import request from "supertest";
-// import { afterAll, expect, it } from "vitest";
-// import { describe } from "node:test";
+import request from "supertest";
+import { afterAll, expect, it } from "vitest";
+import { describe } from "node:test";
 
-// import { tDeleteUser } from "./utils";
-// import app from "../../src/app";
-// const mockUser = {
-//   name: "Super Tester",
-//   email: "supertesting@Test.com",
-//   password: "Test@1234",
-// };
+import { tDeleteUser } from "./utils";
+import app from "../../src/app";
+const mockUser = {
+  name: "Super Tester",
+  email: "supertesting@Test.com",
+  password: "Test@1234",
+};
 
-// afterAll(async () => {
-//   await tDeleteUser(mockUser.email);
-// });
+afterAll(async () => {
+  await tDeleteUser(mockUser.email);
+});
 
-// describe("Testing User Registration/Login Flow", () => {
-//   it("should test registration of a new user", async () => {
-//     await request(app)
-//       .post("/user/register")
-//       .send({
-//         name: mockUser.name,
-//         email: mockUser.email,
-//         password: mockUser.password,
-//       })
-//       .expect(201)
-//       .then((res) => {});
-//   });
+describe("Testing User Registration/Login Flow", () => {
+  it("should test registration of a new user", async () => {
+    await request(app)
+      .post("/user/register")
+      .send({
+        name: mockUser.name,
+        email: mockUser.email,
+        password: mockUser.password,
+      })
+      .expect(201)
+      .then((res) => {});
+  });
 
-//   it("should respond with error when user tries to register with an already registered email", async () => {
-//     await request(app)
-//       .post("/user/register")
-//       .send({
-//         name: mockUser.name,
-//         email: mockUser.email,
-//         password: mockUser.password,
-//       })
-//       .expect(400)
-//       .then((res) => {
-//         const body = res.body;
-//         expect(body).toEqual(
-//           expect.objectContaining({
-//             err_code: expect.any(Number),
-//             err_msg: expect.any(String),
-//           })
-//         );
-//         expect(body["err_code"]).toEqual(1);
-//       });
-//   });
+  it("should respond with error when user tries to register with an already registered email", async () => {
+    await request(app)
+      .post("/user/register")
+      .send({
+        name: mockUser.name,
+        email: mockUser.email,
+        password: mockUser.password,
+      })
+      .expect(400)
+      .then((res) => {
+        const body = res.body;
+        expect(body).toEqual(
+          expect.objectContaining({
+            err_code: expect.any(Number),
+            err_msg: expect.any(String),
+          })
+        );
+        expect(body["err_code"]).toEqual(1);
+      });
+  });
 
-//   it("should test user login with correct credentials", async () => {
-//     await request(app)
-//       .post("/user/login")
-//       .send({ email: mockUser.email, password: mockUser.password })
-//       .expect(200)
-//       .then((res) => {
-//         const body = res.body;
-//         expect(body).toEqual(
-//           expect.objectContaining({
-//             token: expect.any(String),
-//           })
-//         );
-//       });
-//   });
-// });
+  it("should test user login with correct credentials", async () => {
+    await request(app)
+      .post("/user/login")
+      .send({ email: mockUser.email, password: mockUser.password })
+      .expect(200)
+      .then((res) => {
+        const body = res.body;
+        expect(body).toEqual(
+          expect.objectContaining({
+            token: expect.any(String),
+          })
+        );
+      });
+  });
+});
